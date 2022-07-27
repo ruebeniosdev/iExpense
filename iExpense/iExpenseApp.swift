ffdd2ebf31ff4467289d9dff3e37836273d6b9c9
Akankobateng Rueben <akankobatengr@gmail.com>
2022-07-27 14:03:41 +0000

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by RUEBEN on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
